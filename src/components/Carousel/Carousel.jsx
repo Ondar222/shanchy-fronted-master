cd69e76c49b18c135fefce7eb2653c4ad1c2ab30
Ondar222 <Arslanondar2003@gmail.com>
2023-02-09 17:25:24 +0700

    Shanchy_new_commit

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,25 +2,9 @@ import React from 'react';
 import './styles.css'
 
 const Slider = () => {
+
     return (
-        <div className="index-page-slider">
-            {[1, 2, 3].map((item, index) => {
-                return (
-                    <div key={index} className="item">
-                        <div className="image" style={{backgroundImage: `url('https://api.mert.rtyva.ru/uploads/photo2_a5c23bae8b.jpg')`}}>
-                            <img src="/public/Шанчы.jpg"/></div>
-                        <div className="wrap">
-                            <div className="text">
-                                <div className="name">
-                                    Готовая дизайн система государственных сайтов
-                                </div>
-                                <p>Структура и демо-наполнение в соответствие с требованиями федеральных законов</p>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })}
-        </div>
+        <div></div>
     );
 };
 
--- src/components/Posts/PostCard.jsx
+++ src/components/Posts/PostCard.jsx
@@ -1,14 +1,12 @@
 import React from "react";
+import { unixTimeConverter } from "../functions/unixTime.Converter";
 
 const PostCard = (params) => {
   if (!params) {
     return <h3>Empty post</h3>;
   } else {
     return (
-      <div
-        className="post__card"
-        key={params.id}
-      >
+      <div className="post__card" key={params.id}>
         <div className="image-container">
           <img
             alt="some"
@@ -19,32 +17,32 @@ const PostCard = (params) => {
           />
         </div>
 
-
         <div className="content">
-
           <div className="header">
-
             <h1 className="heading">{params.title}</h1>
           </div>
-
           <div className="body">
             <p className="text-md">{params.news_preview}</p>
           </div>
 
-          <div className="footer">         
-              <p className="">Дата публикации:</p>
-              {/* {unixTimeConverter(params.createdAt)} */}
-             <div className="btn">
-            <a href={`/news/post-${params.id}`} className="button dark:bg-gray-500">
-              Читать
-            </a>
-             </div>
+          <div className="footer">
+            <div className="date">
+              <p className="data">Дата публикации: </p>
+              <p className="unix">{unixTimeConverter(params.createdAt)}</p>
+            </div>
+
+            <div className="btn">
+              <a
+                href={`/news/post-${params.id}`}
+                className="button dark:bg-gray-500"
+              >
+                Читать
+              </a>
             </div>
           </div>
         </div>
-     
+      </div>
     );
   }
 };
-
 export default PostCard;
