451875efe93a19b05721c8d36860328b24f868c7
Ondar222 <Arslanondar2003@gmail.com>
2023-02-13 17:33:35 +0700

    Shanchy_new_commit

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
-import './styles.css'
+import React, { useEffect } from "react";
+import "./styles.css";
+import Slider from "react-slick";
+import { height } from "@mui/system";
 
-const Slider = () => {
+const MainpageSlider = (params) => {
+  useEffect(() => {
+    document.getElementsByClassName("slick-next")[0].textContent = ">";
+    document.getElementsByClassName("slick-prev")[0].textContent = "<";
+  }, []);
+  return (
+    // <div className="index-page-slider">
+    <Slider adaptiveHeight>
+      {params.slider.map((item, index) => {
+        return (
+          <div className="item">
+            <div
+              className="image"
+              style={{
+                height: "500px",
+                display: "flex",
+                backgroundImage: `url('${import.meta.env.PUBLIC_URL}${
+                  item.image.url
+                }')`,
+              }}
+            ></div>
 
-    return (
-        <div></div>
-    );
+            <div className="text"
+             
+            >
+              <h1
+                className=""
+                style={{
+                  fontSize: "30px",
+                }}
+              >
+                {item.title}
+              </h1>
+              <p>{item.content}</p>
+            </div>
+          </div>
+        );
+      })}
+    </Slider>
+    // </div>
+  );
 };
 
-export default Slider;
+export default MainpageSlider;
